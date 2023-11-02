43768d63bbc829e707c2c947f527befa6ec13203
madalin_grecescu <ion_madalin.grecescu@mail.schwarz>
2023-11-02 18:32:15 +0200

    added the shopping cart and order endpoints to users microservice documentation, added migration for creating the shopping cart table in the users db

--- internal/db/migrations/pizzeria/000001_init_schema_pizzeria.up.sql
+++ internal/db/migrations/pizzeria/000001_init_schema_pizzeria.up.sql
@@ -5,14 +5,4 @@ CREATE TABLE "pizzas" (
                           "price" DOUBLE PRECISION NOT NULL
 );
 
-CREATE TABLE "pizza_orders" (
-                                "order_id" serial PRIMARY KEY,
-                                "customer_name" VARCHAR(100) NOT NULL,
-                                "customer_phone" VARCHAR(15) NOT NULL,
-                                "pizza_id" INT NOT NULL,
-                                "delivery_address" TEXT NOT NULL,
-                                "order_date" TIMESTAMP DEFAULT 'now()'
-);
-
-
 ALTER TABLE "pizza_orders" ADD FOREIGN KEY ("pizza_id") REFERENCES "pizzas" ("pizza_id");
